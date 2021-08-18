afd94fcbc85cb80df1f89b25c9731848a0c1c319
franmontoya <franciscomontoya000@gmail.com>
2021-08-18 17:27:47 -0300

    Update Product.test.js

--- src/__test__/components/Product.test.js
+++ src/__test__/components/Product.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
 import ProviderMock from '../../__mocks__/ProviderMock';
-import Product from '../../components/product';
+import Product from '../../components/Product';
 import productMock from '../../__mocks__/productMock' 
 
 describe("<product/>", () => {
